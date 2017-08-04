9d806f1b5a1f4d444437194b47c7fc322d10c2e4
Olivier LeDiouris <olivier.lediouris@gmail.com>
2017-08-04 15:58:53 -0700

    Working on GPIO interrupts

--- core/src/java/examples/io/gpio/PinInterrupt.java
+++ core/src/java/examples/io/gpio/PinInterrupt.java
@@ -5,18 +5,26 @@ import jhard.io.GPIO;
 /**
  * Input from a push button
  */
-public class SimpleInput {
+public class PinInterrupt {
 // GPIO numbers refer to different physical pins on various boards
 // On the Raspberry Pi GPIO 4 is physical pin 7 on the header
 // see setup.png in the sketch folder for wiring details
   private int pin = 27;
 
-  public SimpleInput() {
+  public PinInterrupt() {
     this.setup();
   }
 
   private void setup() {
     GPIO.pinMode(pin, GPIO.INPUT);
+    GPIO.attachInterrupt(pin, this, "buttonListener", GPIO.CHANGE);
+
+	  // TODO In the line above, replace the parent and methodName with a Consumer<Integer>
+//  GPIO.attachInterrupt(pin, this::buttonListener, GPIO.CHANGE);
+  }
+
+  private void buttonListener(int i) {
+    System.out.println(String.format("Interrupt! Prm: %d", i));
   }
 
   public void check() {
@@ -28,14 +36,24 @@ public class SimpleInput {
     }
   }
 
-  private static boolean go = true;
   public static void main(String... args) {
-    SimpleInput simpleInput = new SimpleInput();
+
+  	new PinInterrupt();
+
+    final Thread me = Thread.currentThread();
+
     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      go = false;
+      synchronized (me) {
+      	me.notify();
+	    }
     }));
-    while (go) {
-      simpleInput.check();
+
+    synchronized (me) {
+	    try {
+		    me.wait();
+	    } catch (InterruptedException e) {
+		    e.printStackTrace();
+	    }
     }
     System.out.println("Bye");
   }
--- core/src/java/jhard/io/GPIO.java
+++ core/src/java/jhard/io/GPIO.java
@@ -28,6 +28,7 @@ import java.lang.reflect.Method;
 import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 
 /**
@@ -150,14 +151,17 @@ public class GPIO {
     irqThreads.put(pin, t);
   }
 
+  public static void attachInterrupt(int pin, Consumer<Integer> callback, int mode) {
 
-  /**
-   *  Checks if the GPIO pin number can be valid
-   *
-   *  Board-specific classes, such as RPI, assign -1 to pins that carry power,
-   *  ground and the like.
-   *  @param pin GPIO pin
-   */
+  }
+
+    /**
+		 *  Checks if the GPIO pin number can be valid
+		 *
+		 *  Board-specific classes, such as RPI, assign -1 to pins that carry power,
+		 *  ground and the like.
+		 *  @param pin GPIO pin
+		 */
   protected static void checkValidPin(int pin) {
     if (pin < 0) {
       throw new RuntimeException("Operation not supported on this pin");
