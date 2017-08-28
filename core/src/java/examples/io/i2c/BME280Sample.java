73499a946814423e34879e35c2f5bd557398efe9
Olivier LeDiouris <olivier.lediouris@oracle.com>
2017-08-28 16:21:20 -0700

    Housekeeping

--- core/src/java/examples/io/i2c/BME280Sample.java
+++ core/src/java/examples/io/i2c/BME280Sample.java
@@ -14,7 +14,7 @@ public class BME280Sample {
 			System.out.println(String.format("Press: %.02f hPa", (data.getPress() / 100)));
 			System.out.println(String.format("Alt  : %.02f m", data.getAlt()));
 			System.out.println(String.format("Temp : %.02f \272C", data.getTemp()));
-			System.out.println(String.format("Hum  : %.02f %", data.getHum()));
+			System.out.println(String.format("Hum  : %.02f %%", data.getHum()));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
